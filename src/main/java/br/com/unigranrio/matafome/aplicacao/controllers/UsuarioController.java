8f2cc31cb066e63ce5c8efc51d46b337954e54ed
thiagogmiranda <thiagogmiranda.br@gmail.com>
2015-09-14 15:21:10 -0300

    Criado endpoint no UsuarioController para criacao

--- src/main/java/br/com/unigranrio/matafome/aplicacao/controllers/UsuarioController.java
+++ src/main/java/br/com/unigranrio/matafome/aplicacao/controllers/UsuarioController.java
@@ -1,9 +1,12 @@
 package br.com.unigranrio.matafome.aplicacao.controllers;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.unigranrio.matafome.dominio.acoes.CriarUsuario;
 import br.com.unigranrio.matafome.dominio.modelo.Usuario;
 import br.com.unigranrio.matafome.dominio.repositorios.UsuarioRepositorio;
 import br.com.unigranrio.matafome.infra.repositorios.UsuarioRepositorioImpl;
@@ -11,13 +14,20 @@ import br.com.unigranrio.matafome.infra.repositorios.UsuarioRepositorioImpl;
 @RestController()
 public class UsuarioController {
 	private UsuarioRepositorio usuarioRepositorio;
+	private CriarUsuario criarUsuario;
 	
 	public UsuarioController(){
-		this.usuarioRepositorio = new UsuarioRepositorioImpl(); 
+		this.usuarioRepositorio = new UsuarioRepositorioImpl();
+		this.criarUsuario = new CriarUsuario(usuarioRepositorio);
 	}
 	
 	@RequestMapping("/usuario/obter-por-email")
 	public Usuario obterPorEmail(@RequestParam(value = "email")String email){
 		return usuarioRepositorio.obterPorEmail(email);
+	}
+	
+	@RequestMapping(value = "/usuario/criar", method = RequestMethod.POST)
+	public void criarUsuario(@RequestBody Usuario usuario){
+		criarUsuario.Executar(usuario);
 	}
 }
