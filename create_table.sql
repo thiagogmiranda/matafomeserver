70c0aaae42bd79af652f5f81503b0d381a7facb5
thiagogmiranda <thiagogmiranda.br@gmail.com>
2015-11-05 13:28:43 -0200

    Script de Criação de Tabelas

---
+++ create_table.sql
@@ -0,0 +1,32 @@
+CREATE TABLE Usuario (
+	Id serial unique,
+	Login varchar(15) not null,
+	Senha varchar(max) not null,
+	DataCadastro date not null
+);
+
+CREATE TABLE Consumidor (
+	Id serial unique,
+	Nome varchar(100) not null,
+	Email varchar(200) not null,
+	DataCadastro date not null,
+	IdUsuario int not null references Usuario(Id)
+);
+
+CREATE TABLE Negocio (
+	Id serial unique,
+	Nome varchar(100) not null,
+	Descricao varchar(300) not null,
+	Latitude double precision not null,
+	Longitude double precision not null,
+	DataCadastro date not null,
+	IdUsuario int not null references Usuario(Id)
+);
+
+CREATE TABLE Avaliacao (
+	Id serial unique,
+	Comentario varchar(300) not null,
+	Nota int not null,
+	IdUsuario int not null references Usuario(Id),
+	IdVendedor int not null references Vendedor(Id)
+);
