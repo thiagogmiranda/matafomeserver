00def65b296c90317d4a683eb979b156129a253e
thiagogmiranda <thiagogmiranda.br@gmail.com>
2015-11-07 16:52:40 -0200

    Atualização do Script de criação de tabelas

--- create_table.sql
+++ create_table.sql
@@ -1,15 +1,11 @@
 CREATE TABLE Usuario (
 	Id serial unique,
-	Email varchar(200) not null,
-	Senha varchar(max) not null,
-	DataCadastro date not null
-);
-
-CREATE TABLE Consumidor (
-	Id serial unique,
 	Nome varchar(100) not null,
-	IdUsuario int not null references Usuario(Id)
-);
+	Email varchar(200) not null,
+	Senha text not null,
+	Tipo char(2) not null,
+	DataCadastro date not null
+)
 
 CREATE TABLE Negocio (
 	Id serial unique,
@@ -18,12 +14,12 @@ CREATE TABLE Negocio (
 	Latitude double precision not null,
 	Longitude double precision not null,
 	IdUsuario int not null references Usuario(Id)
-);
+)
 
 CREATE TABLE Avaliacao (
 	Id serial unique,
 	Comentario varchar(300) not null,
 	Nota int not null,
 	IdUsuario int not null references Usuario(Id),
-	IdVendedor int not null references Vendedor(Id)
+	IdNegocio int not null references Negocio(Id)
 );
