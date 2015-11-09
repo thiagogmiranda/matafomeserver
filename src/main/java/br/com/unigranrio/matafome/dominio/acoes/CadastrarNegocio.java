3f5675ec09fb813de8b68078f980f93d6028e6ac
thiagogmiranda <thiagogmiranda.br@gmail.com>
2015-11-09 14:19:14 -0200

    Retorno de dados ao cadastrar negocio

--- src/main/java/br/com/unigranrio/matafome/dominio/acoes/CadastrarNegocio.java
+++ src/main/java/br/com/unigranrio/matafome/dominio/acoes/CadastrarNegocio.java
@@ -6,20 +6,24 @@ import br.com.unigranrio.matafome.dominio.validadores.ValidadorCadastroNegocio;
 
 public class CadastrarNegocio {
 
-	private NegocioRepositorio barracaRepositorio;
+	private NegocioRepositorio negocioRepositorio;
 	private ValidadorCadastroNegocio validadorCadastro;
 	
 	public CadastrarNegocio(NegocioRepositorio barracaRepositorio, ValidadorCadastroNegocio validadorCadastro){
-		this.barracaRepositorio = barracaRepositorio;
+		this.negocioRepositorio = barracaRepositorio;
 		this.validadorCadastro = validadorCadastro;
 	}
 	
-	public ResultadoAcao<Negocio> executar(Negocio barraca) {
+	public ResultadoAcao<Negocio> executar(Negocio negocio) {
 		ResultadoAcao<Negocio> resultado = new ResultadoAcao<Negocio>();
-		resultado.adicionarMensagens(validadorCadastro.validar(barraca));
+		resultado.adicionarMensagens(validadorCadastro.validar(negocio));
 		
 		if(resultado.estaValido()){
-			barracaRepositorio.salvar(barraca);
+			negocioRepositorio.salvar(negocio);
+			
+			Negocio salvo = negocioRepositorio.obterPorLatLng(negocio.getLatitude(), negocio.getLongitude());
+			
+			resultado.setData(salvo);
 		}
 		
 		return resultado;
