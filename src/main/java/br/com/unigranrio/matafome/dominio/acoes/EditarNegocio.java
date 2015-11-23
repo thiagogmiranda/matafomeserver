397d66e0dc1f98bf60f1f31051fa3b58126d4102
thiagogmiranda <thiagogmiranda.br@gmail.com>
2015-11-22 22:32:53 -0200

    Implementação do caso de uso para editar um negócio

--- src/main/java/br/com/unigranrio/matafome/dominio/acoes/EditarNegocio.java
+++ src/main/java/br/com/unigranrio/matafome/dominio/acoes/EditarNegocio.java
@@ -1,5 +1,32 @@
 package br.com.unigranrio.matafome.dominio.acoes;
 
-public class EditarNegocio {
+import br.com.unigranrio.matafome.dominio.modelo.Negocio;
+import br.com.unigranrio.matafome.dominio.repositorios.NegocioRepositorio;
+import br.com.unigranrio.matafome.dominio.validadores.ValidadorEdicaoNegocio;
 
+public class EditarNegocio {
+	private NegocioRepositorio repositorio;
+	private ValidadorEdicaoNegocio validador;
+	
+	public EditarNegocio(NegocioRepositorio repositorio, ValidadorEdicaoNegocio validador){
+		this.repositorio = repositorio;
+		this.validador = validador;
+	}
+	
+	public ResultadoAcao<Negocio> executar(Negocio negocio){
+		ResultadoAcao<Negocio> resultado = new ResultadoAcao<Negocio>();
+		resultado.adicionarMensagens(validador.validar(negocio));
+		
+		if(resultado.estaValido()){
+			Negocio salvo = repositorio.obterPorId(negocio.getId());
+			
+			salvo.setDescricao(negocio.getDescricao());
+			salvo.setLatitude(negocio.getLatitude());
+			salvo.setLongitude(negocio.getLongitude());
+			
+			repositorio.atualizar(salvo);
+		}
+		
+		return resultado;
+	}
 }
